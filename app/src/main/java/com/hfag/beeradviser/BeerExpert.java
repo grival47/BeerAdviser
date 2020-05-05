301cd23c0a375336dfce7df021a5de424e1a95c4
Valery <gvv_47@mail.ru>
2020-05-05 22:16:29 +0300

    amber-Amber

--- app/src/main/java/com/hfag/beeradviser/BeerExpert.java
+++ app/src/main/java/com/hfag/beeradviser/BeerExpert.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class BeerExpert {
     List<String> getBrands(String color) {
         List<String> brands = new ArrayList<>();
-        if (color.equals("amber")) {
+        if (color.equals("Amber")) {
             brands.add("Jack Amber");
             brands.add("Red Moose");
         } else {
